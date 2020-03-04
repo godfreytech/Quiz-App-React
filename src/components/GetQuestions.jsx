01b3b94b439a64ec134f3ea00dbd4f35015780dd
ShubhamVerma1811 <imshubhamverma.sv@gmail.com>
2020-03-04 20:16:01 +0530

    Added choices array

--- src/components/GetQuestions.jsx
+++ src/components/GetQuestions.jsx
@@ -7,21 +7,26 @@ export class GetQuestions extends Component {
     // this.nextQuestion = this.nextQuestion.bind(this);
     this.index = 1;
     this.state = {
-      question: "loading..",
+      question: "Loading..",
       questionsList: [],
+      incorrectAnswers: [],
+      correctAnswer: [],
       choices: []
     };
   }
 
   componentDidMount() {
-    console.log(this.state.questionsList);
     fetch("https://opentdb.com/api.php?amount=10")
       .then(response => response.json())
       .then(data => {
         console.log(data.results);
+
+        //setting state of the first question[0]
         this.setState({
           question: data.results[0].question
         });
+
+        // pushing all questions in questionsList[]
         for (let item in data.results) {
           this.setState({
             questionsList: [
@@ -30,15 +35,42 @@ export class GetQuestions extends Component {
             ]
           });
         }
-        return this.state.questionsList;
-        // });
+
+        //getting incorrectAnswers and correctAnswer and pushing them in the choices[]
+        for (let item in data.results) {
+          this.setState({
+            incorrectAnswers: [
+              ...this.state.incorrectAnswers,
+              data.results[item].incorrect_answers
+            ],
+            correctAnswer: [
+              ...this.state.correctAnswer,
+              data.results[item].correct_answer
+            ]
+          });
+        }
+
+        for (let item in data.results) {
+          this.setState({
+            choices: [
+              ...this.state.choices,
+              this.state.incorrectAnswers[item].concat(
+                this.state.correctAnswer[item]
+              )
+            ]
+          });
+        }
+
+        console.log(this.state.choices);
+
+        return null;
       });
   }
 
   // nextQuestion() {
   //   if (this.state.questionsList.length) {
   //     this.setState({
-  //       question: this.state.questionsList[this.index]
+  //       question: this.state.questionsList[this.index],
   //     });
   //     this.index++;
   //   }
